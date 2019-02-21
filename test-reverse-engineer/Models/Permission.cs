6a70316d5ceca387079beaea150ed4d8d0af8333
Dinan Rangga Maulana <dinanranggamaulana@gmail.com>
2019-02-21 08:36:44 +0700

    Create the Entitiy Framework model based on my existing database.

---
+++ test-reverse-engineer/Models/Permission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test_reverse_engineer.Models
+{
+    public partial class Permission
+    {
+        public Permission()
+        {
+            RoleHasPermission = new HashSet<RoleHasPermission>();
+        }
+
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; }
+        public DateTime PermissionCreatedDate { get; set; }
+        public DateTime PermissionModifiedDate { get; set; }
+
+        public virtual ICollection<RoleHasPermission> RoleHasPermission { get; set; }
+    }
+}
---
+++ test-reverse-engineer/Models/Plan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test_reverse_engineer.Models
+{
+    public partial class Plan
+    {
+        public Plan()
+        {
+            ProjectNavigation = new HashSet<Project>();
+        }
+
+        public int PlanId { get; set; }
+        public int? ProjectId { get; set; }
+        public string PlanLessonLearned { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual ICollection<Project> ProjectNavigation { get; set; }
+    }
+}
