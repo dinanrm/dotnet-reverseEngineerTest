31ffb5997f6e3338d338ad68126152c66b95faf1
Dinan Rangga Maulana <dinanranggamaulana@gmail.com>
2019-02-21 08:17:11 +0700

    Add project files.

---
+++ test-reverse-engineer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_reverse_engineer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
